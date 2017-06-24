f314a44257ecfb56c11f21c4cf387811a30b28bf
KimJiEons <kakasky112@naver.com>
2017-06-24 14:45:04 +0900

    test

--- DynamicBeat/src/GitTestPack/HelloWorld.java
+++ DynamicBeat/src/GitTestPack/HelloWorld.java
@@ -8,7 +8,7 @@ public class HelloWorld {
 		
 		System.out.println("Git Test");
 		
-		System.out.println("Git Test");System.out.println("Git Test");
+		System.out.println("Git Test");System.out.println("Git sdTest");
 
 	}
 
